32e6abc7d137a55afe5dafcdf66aac99e5ca9ae4
K0N5TI <konsti.wohner@outlook.de>
2021-02-17 12:41:07 +0100

    Raplaced absolute path wirh relative path

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ from flask_oidc import OpenIDConnect
 def init_app():
     """Create Flask application."""
     app = Flask(__name__, static_folder='/static')
-    app.config.update({'SECRET_KEY': 'u\x91\xcf\xfa\x0c\xb9\x95\xe3t\xba2K\x7f\xfd\xca\xa3\x9f\x90\x88\xb8\xee\xa4\xd6\xe4',
-                       'OIDC_CLIENT_SECRETS': 'C:\VisualStudio\Flask-Keycloak\client_secrets.json',
-                       'OIDC_ID_TOKEN_COOKIE_SECURE': False,
-                       'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/Application1'],
-                       'OIDC_SCOPES': ['openid', 'email', 'profile'],
-                       'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
-                       'OIDC_RESOURCE_CHECK_AUD': True,  # Audience
-                       'OIDC_CLOCK_SKEW': 560
-    })
+    app.config.update({
+                          'SECRET_KEY': 'u\x91\xcf\xfa\x0c\xb9\x95\xe3t\xba2K\x7f\xfd\xca\xa3\x9f\x90\x88\xb8\xee\xa4\xd6\xe4',
+                          'OIDC_CLIENT_SECRETS': '.\client_secrets.json',
+                          'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+                          'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/Application1'],
+                          'OIDC_SCOPES': ['openid', 'email', 'profile'],
+                          'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
+                          'OIDC_RESOURCE_CHECK_AUD': True,  # Audience
+                          'OIDC_CLOCK_SKEW': 560
+                          })
 
     with app.app_context():
         from Auth.LoginView import auth
